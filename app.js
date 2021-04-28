83c532c0f65ce854462c16c62bae2b2eddbd67b8
Thaís Mendes Ramalho <thaismr@users.noreply.github.com>
2021-04-28 11:49:46 -0300

    init commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello from a sample AWS pipeline app!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
