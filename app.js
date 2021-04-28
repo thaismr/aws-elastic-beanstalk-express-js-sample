709f6d0afef2630d4532420f870515d4a42b7088
Thaís Mendes Ramalho <thaismr@users.noreply.github.com>
2021-04-28 17:57:13 -0300

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello from a sample AWS pipeline app!'));
+app.get('/', (req, res) => res.send('Hello again, from a sample AWS pipeline app!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
